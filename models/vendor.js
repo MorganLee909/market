244b4951fe658b8759c15c705a097d430f98f8b8
Lee Morgan <Morgan.LeeR@protonmail.com>
2020-10-31 17:45:53 +0300

    Create vendor model.

---
+++ models/vendor.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const VendorSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    ownerName: String,
+    address: String,
+    geoLocation: "Point",
+    url: String,
+    description: String,
+    items: [{
+        name: {
+            type: String,
+            required: true
+        },
+        quantity: {
+            type: String,
+            required: true
+        },
+        unit: {
+            type: String,
+            required: true
+        }
+    }]
+});
+
+module.exports = mongoose.model("Vendor", VendorSchema);
