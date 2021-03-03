351e8eeb95ebe5bd3295478d706382421d4c25ef
ivan kordonets <ivankordonets@ivans-MacBook-Pro.local>
2021-03-03 10:15:42 +0200

    finish modal window for remove products

--- views/index.js
+++ views/index.js
@@ -44,9 +44,6 @@ controller = {
     openModal: function( modal, data = {} ){
         document.getElementById("modal").style.display = "flex";
         document.getElementById( modal ).style.display = "flex";
-        
-        let subTitle = document.getElementById("confSubtitleModal");
-        subTitle.innerText = `${data.item.getAttribute('amount')} Kg of ${data.item.getAttribute('product')} ${"will be removed"}`;
 
         switch( modal ){
             case 'confirmationModal':
--- views/js/modal.js
+++ views/js/modal.js
@@ -5,6 +5,9 @@ let modal = {
         var cancelBtn = document.getElementById("confirmationCancelBtn");
         var confBtn = document.getElementById("confirmationConfirmBtn");
 
+        let subTitle = document.getElementById("confSubtitleModal");
+        subTitle.innerText = `${ item.getAttribute('amount')} Kg of ${ item.getAttribute('product')} ${"will be removed"}`;
+
         cancelBtn.onclick = function () {
             controller.closeModal();
         };
